4ea1e49993d39cb5613a8f1e28ddf5a0c4ace6d9
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-04-17 11:02:27 +0530

    back end connection part1

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,12 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongo://localhost:27017/thriftecom",{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then((data)=>{
-console.log('Mongodb connected with server: ${data.connection.host}');
-}).catch((err)=>{
-    console.log(err);
-})
+const connectDatabase = ( ) =>{
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+        console.log('Mongodb connected with server: ${data.connection.host}');
+        }).catch((err)=>{
+            console.log(err);
+        })
+}
+
+module.exports = connectDatabase
