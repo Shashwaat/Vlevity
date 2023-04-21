8031b64e0ecf1362dc7d2631903a5bb43c6a11f8
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-04-21 11:07:33 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const connectDatabase = require("./config/database")
+const connectDatabase = require("./config/database") // including necessary files to use config database
 //config
 dotenv.config({path:"backend/config/config.env"})
 
 // Connecting to database of mongodb using databsejs file
 connectDatabase()
 
+
 //listening the event from the port
 app.listen(process.env.PORT,()=>{
-    console.log('Server is working on http://localhost:${process.env.PORT}')
+    console.log('Server is working on http://localhost:${process.env.PORT}') // it will redirect to the local host env port
 })
