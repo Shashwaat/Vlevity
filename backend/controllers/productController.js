581beb101cfd8c0a4283fc229dc87db27e75c592
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-04-24 21:50:28 +0530

    initial commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 
 //to get all the products request and response
 exports.getAllProducts = (req,res)=>{
-    //route status 
+    //route status that if it's working fine or not
     res.status(200).json({message:"Route is working fine"})
 }
