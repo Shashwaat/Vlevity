dad42e61313a952120c6d62c3abffe8536ceb922
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-05-01 21:08:51 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const connectDatabase = require("./config/database") // including necessary files to use config database
+const connectDatabase = require("./config/database") // include necessary files to use config database
 dotenv.config({path:"backend/config/config.env"})
 connectDatabase()
 
 
 //listening the event from the port
 app.listen(process.env.PORT,()=>{
-    console.log('Server is working on http://localhost:${process.env.PORT}') // it will redirect to the local host env port
+    console.log('Server is working on http://localhost:${process.env.PORT}') // it will let you to the local host env port
 })
