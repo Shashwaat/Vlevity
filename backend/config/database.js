01b26df67d53ff8ff38b9d1978ba3744c30a7b86
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-05-07 22:12:50 +0530

    backend updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,8 @@
-//to use mongoose we'll use this 
+//to use mongoose we'll 
 const mongoose = require("mongoose");
 
-//function to connect data
 const connectDatabase = ( ) =>{
-    //use of 
+    
     mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
         console.log('Mongodb connected with server: ${data.connection.host}');
         }).catch((err)=>{
